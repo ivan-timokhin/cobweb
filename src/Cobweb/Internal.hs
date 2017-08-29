705e1083a70799ef62f20f18947fdf0c16c2bc9b
Ivan Timokhin <timokhin.iv@gmail.com>
2017-08-29 22:41:09 +0300

    Add MonadState, MonadReader and MonadError instances (from mtl)

--- src/Cobweb/Internal.hs
+++ src/Cobweb/Internal.hs
@@ -1,10 +1,12 @@
-{-# LANGUAGE DeriveFunctor #-}
-{-# LANGUAGE UndecidableInstances #-}
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE StandaloneDeriving #-}
-{-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 module Cobweb.Internal
   ( NodeF(ReturnF, EffectF, ConnectF)
@@ -17,9 +19,12 @@ module Cobweb.Internal
   ) where
 
 import Control.Monad (ap)
+import Control.Monad.Except (MonadError(catchError, throwError))
 import qualified Control.Monad.Fail as Fail
 import Control.Monad.IO.Class (MonadIO(liftIO))
 import Control.Monad.Morph (MFunctor(hoist), MMonad(embed))
+import Control.Monad.Reader.Class (MonadReader(ask, local, reader))
+import Control.Monad.State.Class (MonadState(get, put, state))
 import Control.Monad.Trans (MonadTrans(lift))
 import Data.Bifunctor (Bifunctor(first, second))
 import Data.Functor.Foldable (Base, Recursive(cata, project))
@@ -79,6 +84,41 @@ instance (All Functor cs, MonadIO m) => MonadIO (Node cs m) where
 
 instance (All Functor cs, Fail.MonadFail m) => Fail.MonadFail (Node cs m) where
   fail = lift . Fail.fail
+
+instance (All Functor cs, MonadError e m) => MonadError e (Node cs m) where
+  throwError = lift . throwError
+  catchError node handler = loop node
+    where
+      loop n =
+        Node $
+        case getNode n of
+          ReturnF r -> ReturnF r
+          ConnectF con -> ConnectF (fmap loop con)
+          EffectF eff -> EffectF $ fmap loop eff `catchError` (pure . handler)
+
+instance (All Functor cs, MonadReader r m) => MonadReader r (Node cs m) where
+  ask = lift ask
+  reader = lift . reader
+  -- This relies on `local f` being proper monad morphism… which is
+  -- unknowable, since mtl doesn't have any laws on its
+  -- classes. *grumble*
+  local f = unsafeHoist (local f)
+
+-- MonadWrite is absent primarily because of listen; the problem is
+-- that, since we have an entire list of monadic actions, it is
+-- necessary to listen on each individually, and then manually combine
+-- the results.  That wouldn't be too bad, but there are two ways to
+-- do that—strict and lazy—and while the usual preference is to be as
+-- strict as possible, I just know there's someone out there who
+-- relies on this laziness for some clever recursive-knot-tying, and
+-- whose code will be irreparably broken by introducing strict operations.
+--
+-- The moral of the story is that if it is impossible to do correctly,
+-- I'd rather not do it at all.
+instance (All Functor cs, MonadState s m) => MonadState s (Node cs m) where
+  get = lift get
+  put = lift . put
+  state = lift . state
 
 instance All Functor cs => MFunctor (Node cs) where
   hoist f = unsafeHoist f . observe
