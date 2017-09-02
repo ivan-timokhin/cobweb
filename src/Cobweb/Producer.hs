20fb8ddca6f4c843192e0ae6d735936bfce6c9e5
Ivan Timokhin <timokhin.iv@gmail.com>
2017-09-03 00:07:34 +0300

    Document Cobweb.Producer

--- src/Cobweb/Producer.hs
+++ src/Cobweb/Producer.hs
@@ -1,19 +1,40 @@
+{-|
+Module: Cobweb.Producer
+Description: Nodes that only yield values.
+Copyright: 2017 © Ivan Timokhin
+License: BSD3
+Maintainer: timokhin.iv@gmail.com
+Stability: experimental
+
+Nodes that only yield values.
+
+Some of the functions in this module have channel- and functor-generic
+counterparts in "Cobweb.Core"; these are specialised for 'Producer's.
+-}
+{-# OPTIONS_HADDOCK show-extensions #-}
 {-# LANGUAGE DataKinds #-}
 
-module Cobweb.Producer where
+module Cobweb.Producer
+  ( Producer
+  , each
+  , for
+  ) where
 
-import Cobweb.Core (Yielding, eachOn, forOn)
+import Cobweb.Core (Leaf, Yielding, eachOn, forOn)
 import Cobweb.Internal (Node)
 import Cobweb.Type.Combinators (All, i0)
 
-type Producer a = Node '[ Yielding a]
+-- | A 'Node' that only yields values on its sole open channel.
+type Producer a = Leaf (Yielding a)
 
+-- | Yield each value in the foldable container.
 each :: (Foldable f, Functor m) => f a -> Producer a m ()
 each = eachOn i0
 
+-- | Loop over a producer.
 for ::
      (All Functor cs, Functor m)
-  => Producer a m r
-  -> (a -> Node cs m ())
+  => Producer a m r -- ^ Source of values.
+  -> (a -> Node cs m ()) -- ^ Loop body.
   -> Node cs m r
 for = forOn i0
