c61c128437d6c1ec4a7818050029cf668640badb
Ivan Timokhin <timokhin.iv@gmail.com>
2017-09-03 00:32:48 +0300

    Add a produceOn function

--- src/Cobweb/Producer.hs
+++ src/Cobweb/Producer.hs
@@ -18,11 +18,13 @@ module Cobweb.Producer
   ( Producer
   , each
   , for
+  , produceOn
   ) where
 
-import Cobweb.Core (Leaf, Yielding, eachOn, forOn)
+import Cobweb.Core (mapsAll, Leaf, Yielding, eachOn, forOn)
 import Cobweb.Internal (Node)
-import Cobweb.Type.Combinators (All, i0)
+import Cobweb.Type.Combinators
+       (All, IIndex, finjectIdx, fsumOnly, i0)
 
 -- | A 'Node' that only yields values on its sole open channel.
 type Producer a = Leaf (Yielding a)
@@ -38,3 +40,30 @@ for ::
   -> (a -> Node cs m ()) -- ^ Loop body.
   -> Node cs m r
 for = forOn i0
+
+-- | Embed a 'Producer' into a larger 'Node', by identifying its sole
+-- output channel with a matching channel in the outer 'Node'.
+--
+-- ====__Signatures for some specific indices__
+-- @
+-- 'produceOn' 'i0' ::
+--       'Functor' m
+--    => 'Producer' a m r
+--    -> 'Node' ('Yielding' a : cs) m r
+--
+-- 'produceOn' 'Cobweb.Core.i1' ::
+--       'Functor' m
+--    => 'Producer' a m r
+--    -> 'Node' (c0 : 'Yielding' a : cs) m r
+--
+-- 'produceOn' 'Cobweb.Core.i2' ::
+--       'Functor' m
+--    => 'Producer' a m r
+--    -> 'Node' (c0 : c1 : 'Yielding' a : cs) m r
+-- @
+produceOn ::
+     Functor m
+  => IIndex n cs (Yielding a) -- ^ A channel to attach to.
+  -> Producer a m r
+  -> Node cs m r
+produceOn n = mapsAll (finjectIdx n . fsumOnly)
