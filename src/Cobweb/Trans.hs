17a51bbbd367a505736f8220743464ebdc024bf6
Ivan Timokhin <timokhin.iv@gmail.com>
2017-09-03 21:16:50 +0300

    Explain distribute to myself
    It turns out that 'distribute' is essentially 'run' in disguise (in
    fact, it is implemented that way in pipes).  I have considered making
    the following function, which factors out the common structure:
    
    unsafeInterpret ::
         (Functor m, Monad n, All Functor cs)
      => (forall x. m x -> n x)
      -> (forall x. FSum cs x -> n x)
      -> Node cs m r
      -> n r
    unsafeInterpret e c = cata alg
      where
        alg (ReturnF r) = pure r
        alg (EffectF eff) = join (e eff)
        alg (ConnectF con) = join (c con)
    
    and then
    
    run = unsafeInterpret id (absurd . nilFSum)
    distribute = unsafeInterpret (hoist lift) (lift . connects)
    
    but if anything, this seems /more/ mysterious, as the names of the
    constructors are gone, and matching passed functions to them requires
    noticeable mental effort.  Better large function that is easier to
    follow, than one-liner that is difficult to understand.
    
    As I was writing it, I realised that this is just 'foldFree' from
    ‘free’ package.

--- src/Cobweb/Trans.hs
+++ src/Cobweb/Trans.hs
@@ -11,12 +11,6 @@ import Cobweb.Internal
        (Node, NodeF(ConnectF, EffectF, ReturnF), cata)
 import Cobweb.Type.Combinators (All)
 
--- FIXME figure out what it does and, depending on results,
--- a) rewrite it correctly;
--- b) rewrite it in a more understandable fashion, if it turns out to
--- be correct;
--- c) if none of the above applies/helps, at least explain what
--- happens here in comments.
 distribute ::
      forall m t cs r.
      ( Monad m
@@ -30,7 +24,22 @@ distribute ::
   -> t (Node cs m) r
 distribute = cata alg
   where
+    -- What happens here is that we're trying to interpret our
+    -- existing stack in a new monad.  Joins are essentially plumbing;
+    -- compare with 'run'; the way 'cata' works, we essentially
+    -- convert layer-by-layer, starting from the bottom.  So when we
+    -- convert the next layer, we smash it together with what is
+    -- already done underneath it via 'join'.
     alg :: NodeF cs (t m) r (t (Node cs m) r) -> t (Node cs m) r
+    -- Pure values are easy.
     alg (ReturnF r) = pure r
+    -- We have an effect in (t m), and want an effect in
+    -- (t (Node cs m)).  We essentially want to insert another
+    -- transformer layer right beneath @t@, which is /exactly/ what
+    -- 'hoist' was created for.
     alg (EffectF eff) = join $ hoist lift eff
-    alg (ConnectF con) = join $ lift $ connects con
+    -- We need to make the same connection in t (Node cs m).  That is
+    -- easy; we just use 'connects' to get @Node cs m@ (because
+    -- 'connects' is polymorphic in the base monad), and then lift it
+    -- to @t (Node cs m)@.
+    alg (ConnectF con) = join $ lift (connects con)
