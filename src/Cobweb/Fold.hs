cbf2533541ba15608226e80a4ceb9d2dab32e240
Ivan Timokhin <timokhin.iv@gmail.com>
2017-09-11 21:16:25 +0300

    Flesh out Fold
    I am not sure whether everything is strict enough—I suppose benchmarks
    should tell—but seems fine so far.
    
    I had a long list of specific folds (sum, product, all, that stuff)
    but eventually deleted them; they're kind of trivial to write by hand
    if needed, are already provided by foldl package if which this module
    strongly hints about, and with four variants of each there were a
    /lot/ of them, which looked daunting in the docs, caused a lot of
    repetition in the code, and caused me to procrastinate for
    impressively long.  Screw all that.

--- src/Cobweb/Fold.hs
+++ src/Cobweb/Fold.hs
@@ -1,27 +1,114 @@
+{-|
+Module: Cobweb.Fold
+Description: Folding 'Producer's
+Copyright: 2017 © Ivan Timokhin
+License: BSD3
+Maintainer: timokhin.iv@gmail.com
+Stability: experimental
+
+Functions that fold over a 'Producer'.
+
+Folding functions are designed to be used with the @foldl@ library.
+For example,
+
+@
+Control.Foldl.purely 'foldNode' :: Fold a b -> Producer a m r -> m (b, r)
+Control.Foldl.impurely 'foldMNode' :: FoldM a b -> Producer a m r -> m (b, r)
+@
+
+However, they can certainly be used by themselves.
+
+Note that this module support almost exclusively strict left folds, so
+that the whole process happens in constant space.
+-}
 {-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
-module Cobweb.Fold where
+module Cobweb.Fold
+  (
+    -- * Complete generic folds
+    foldNode
+  , foldNode_
+  , foldMNode
+  , foldMNode_
+    -- * Incomplete folds
+  , foldOn
+  , foldOn_
+  , foldMOn
+  , foldMOn_
+    -- * Scans
+  , scanOn
+  , scanOnM
+    -- * Collecting to list
+    -- $list
+  , toListN
+  , toListN_
+  ) where
 
-import Cobweb.Core (Yielding, run)
+import Control.Monad.Trans (lift)
+import Data.Proxy (Proxy(Proxy))
+
+import Cobweb.Core (Producer, Yielding, run, yieldOn)
 import Cobweb.Internal
        (Node(Node, getNode), NodeF(ConnectF, EffectF, ReturnF))
-import Cobweb.Producer (Producer)
 import Cobweb.Type.Combinators
-       (All, IIndex, IWithout, fdecompIdx, i0)
+       (All, IIndex, IReplace, IReplaced, IWithout, fdecompIdx,
+        fdecompReplaceIdx, i0, ireplace, replaceIdx)
 
+-- | Fold over values from a 'Producer', and provide both the fold
+-- result and the 'Producer' return value.
+foldNode ::
+     Monad m
+  => (x -> a -> x) -- ^ Combining function.
+  -> x -- ^ Seed value.
+  -> (x -> b) -- ^ Finalise accumulator (typically 'id' in manual
+              -- use).
+  -> Producer a m r -- ^ Source of values; essentially an effectful
+                    -- list.
+  -> m (b, r)
+foldNode comb seed fin = run . foldOn i0 comb seed fin
+
+-- | Same as 'foldNode', but discard 'Producer' return value.
+foldNode_ :: Monad m => (x -> a -> x) -> x -> (x -> b) -> Producer a m r -> m b
+foldNode_ comb seed fin = fmap fst . foldNode comb seed fin
+
+-- | Fold over values from a 'Producer', possibly with effects in the
+-- base monad, and provide both the fold result and the 'Producer'
+-- return value.
+--
+-- __Note:__ the action for seed is always run first, prior to
+-- anything in the 'Producer'.
+foldMNode ::
+     Monad m
+  => (x -> a -> m x) -- ^ Combining function.
+  -> m x -- ^ Action yielding a seed value.
+  -> (x -> m b) -- ^ Finalise accumulator (e.g. 'pure').
+  -> Producer a m r
+  -> m (b, r)
+foldMNode comb seed fin = run . foldMOn i0 comb seed fin
+
+-- | Same as 'foldMNode', but discard 'Producer' return value.
+foldMNode_ ::
+     Monad m => (x -> a -> m x) -> m x -> (x -> m b) -> Producer a m r -> m b
+foldMNode_ comb seed fin = fmap fst . foldMNode comb seed fin
+
+-- | Fold over values provided on one of the 'Node' channels, and add
+-- the result to the return value.
 foldOn ::
      (Functor m, IWithout n cs cs', All Functor cs')
-  => (x -> a -> x)
-  -> x
-  -> (x -> b)
-  -> IIndex n cs (Yielding a)
+  => IIndex n cs (Yielding a) -- ^ The index of the channel to fold
+                              -- over.
+  -> (x -> a -> x) -- ^ Combining function.
+  -> x -- ^ Seed.
+  -> (x -> b) -- ^ Finalise accumulator.
   -> Node cs m r
   -> Node cs' m (b, r)
-foldOn comb seed fin n = loop seed
+foldOn n comb seed fin = loop seed
   where
-    loop !z (Node web) =
+    loop !z (Node node) =
       Node $
-      case web of
+      case node of
         ReturnF r -> ReturnF (fin z, r)
         EffectF eff -> EffectF (fmap (loop z) eff)
         ConnectF con ->
@@ -29,15 +116,30 @@ foldOn comb seed fin n = loop seed
             Right (a, rest) -> getNode $ loop (comb z a) rest
             Left other -> ConnectF (fmap (loop z) other)
 
+-- | Same as 'foldOn', but discard original return value.
+foldOn_ ::
+     (Functor m, IWithout n cs cs', All Functor cs')
+  => IIndex n cs (Yielding a)
+  -> (x -> a -> x)
+  -> x
+  -> (x -> b)
+  -> Node cs m r
+  -> Node cs' m b
+foldOn_ n comb seed fin = fmap fst . foldOn n comb seed fin
+
+-- | Same as 'foldOn', but with effects in the base monad.
+--
+-- __Note:__ the action for seed is always run first, prior to
+-- anything in the 'Node'.
 foldMOn ::
      (Functor m, IWithout n cs cs', All Functor cs')
-  => (x -> a -> m x)
+  => IIndex n cs (Yielding a)
+  -> (x -> a -> m x)
   -> m x
   -> (x -> m b)
-  -> IIndex n cs (Yielding a)
   -> Node cs m r
   -> Node cs' m (b, r)
-foldMOn comb seed fin n node' = Node (EffectF (fmap (flip loop node') seed))
+foldMOn n comb seed fin node' = Node (EffectF (fmap (flip loop node') seed))
   where
     loop !z (Node node) =
       Node $
@@ -49,15 +151,155 @@ foldMOn comb seed fin n node' = Node (EffectF (fmap (flip loop node') seed))
             Left other -> ConnectF (fmap (loop z) other)
             Right (a, rest) -> EffectF (fmap (flip loop rest) (comb z a))
 
-foldOnly ::
-     Monad m => (x -> a -> x) -> x -> (x -> b) -> Producer a m r -> m (b, r)
-foldOnly comb seed fin = run . foldOn comb seed fin i0
-
-foldMOnly ::
-     Monad m
-  => (x -> a -> m x)
+-- | Same as 'foldMOn', but discard original return value.
+foldMOn_ ::
+     (Functor m, IWithout n cs cs', All Functor cs')
+  => IIndex n cs (Yielding a)
+  -> (x -> a -> m x)
   -> m x
   -> (x -> m b)
-  -> Producer a m r
-  -> m (b, r)
-foldMOnly comb seed fin = run . foldMOn comb seed fin i0
+  -> Node cs m r
+  -> Node cs' m b
+foldMOn_ n comb seed fin = fmap fst . foldMOn n comb seed fin
+
+-- | Strict left scan of a 'Node' on one of the channels
+-- (cf. 'Data.List.scanl').
+--
+-- The channel producing intermediate values is substituted in-place
+-- for the channel being folded.
+--
+-- __Note:__ seed is always yielded first, before running any action
+-- from the original node.  In general, values are yielded as soon as
+-- they become available.
+--
+-- ====__Signatures for some specific indices__
+-- @
+-- 'scanOn' 'i0' ::
+--      ('Functor' m, 'All' 'Functor' cs)
+--   => (x -> a -> x)
+--   -> x
+--   -> (x -> b)
+--   -> 'Node' ('Yielding' a : cs) m r
+--   -> 'Node' ('Yielding' b : cs) m r
+--
+-- 'scanOn' 'Cobweb.Core.i1' ::
+--      ('Functor' m, 'Functor' c0, 'All' 'Functor' cs)
+--   => (x -> a -> x)
+--   -> x
+--   -> (x -> b)
+--   -> 'Node' (c0 : 'Yielding' a : cs) m r
+--   -> 'Node' (c0 : 'Yielding' b : cs) m r
+--
+-- 'scanOn' 'Cobweb.Core.i2' ::
+--      ('Functor' m, 'Functor' c0, 'Functor' c1, 'All' 'Functor' cs)
+--   => (x -> a -> x)
+--   -> x
+--   -> (x -> b)
+--   -> 'Node' (c0 : c1 : 'Yielding' a : cs) m r
+--   -> 'Node' (c0 : c1 : 'Yielding' b : cs) m r
+-- @
+scanOn ::
+     forall m cs cs' n x a b r.
+     (Functor m, IReplaced n cs (Yielding b) cs', All Functor cs')
+  => IIndex n cs (Yielding a) -- ^ Index of the channel to scan over.
+  -> (x -> a -> x) -- ^ Combining function.
+  -> x -- ^ Seed.
+  -> (x -> b) -- ^ Finalise accumulator (evaluated at every step to
+     -- produce yielded value).
+  -> Node cs m r
+  -> Node cs' m r
+scanOn n comb seed fin = (yieldOn n' (fin seed) >>) . loop seed
+  where
+    n' = replaceIdx (ireplace :: IReplace n cs (Yielding b) cs')
+    loop !z (Node node) =
+      Node $
+      case node of
+        ReturnF r -> ReturnF r
+        EffectF eff -> EffectF (fmap (loop z) eff)
+        ConnectF con ->
+          case fdecompReplaceIdx n (Proxy :: Proxy (Yielding b)) con of
+            Left other -> ConnectF (fmap (loop z) other)
+            Right (a, rest) ->
+              getNode $ do
+                let !z' = comb z a
+                yieldOn n' (fin z')
+                loop z' rest
+
+-- | Same as 'scanOn', but with possible effects in the base monad.
+--
+-- __Note:__ like in 'scanOn', values are yielded as soon as
+-- possible.  In particular, seed is evaluated and yielded before
+-- running anything in the original 'Node'.
+--
+-- ====__Signatures for some specific indices__
+-- @
+-- 'scanOnM' 'i0' ::
+--      ('Monad' m, 'All' 'Functor' cs)
+--   => (x -> a -> m x)
+--   -> m x
+--   -> (x -> m b)
+--   -> 'Node' ('Yielding' a : cs) m r
+--   -> 'Node' ('Yielding' b : cs) m r
+--
+-- 'scanOnM' 'Cobweb.Core.i1' ::
+--      ('Monad' m, 'Functor' c0, 'All' 'Functor' cs)
+--   => (x -> a -> m x)
+--   -> m x
+--   -> (x -> m b)
+--   -> 'Node' (c0 : 'Yielding' a : cs) m r
+--   -> 'Node' (c0 : 'Yielding' b : cs) m r
+--
+-- 'scanOnM' 'Cobweb.Core.i2' ::
+--      ('Monad' m, 'Functor' c0, 'Functor' c1, 'All' 'Functor' cs)
+--   => (x -> a -> m x)
+--   -> m x
+--   -> (x -> m b)
+--   -> 'Node' (c0 : c1 : 'Yielding' a : cs) m r
+--   -> 'Node' (c0 : c1 : 'Yielding' b : cs) m r
+-- @
+scanOnM ::
+     forall m cs cs' n x a b r.
+     (Monad m, IReplaced n cs (Yielding b) cs', All Functor cs')
+  => IIndex n cs (Yielding a) -- ^ Index of the channel to scan over.
+  -> (x -> a -> m x) -- ^ Combining function.
+  -> m x -- ^ Seed.
+  -> (x -> m b) -- ^ Finalising function (called for each intermediate
+                -- result).
+  -> Node cs m r
+  -> Node cs' m r
+scanOnM n comb seed fin node = do
+  !seed' <- lift seed
+  b <- lift $ fin seed'
+  yieldOn n' b
+  loop seed' node
+  where
+    n' = replaceIdx (ireplace :: IReplace n cs (Yielding b) cs')
+    loop !z (Node node') =
+      Node $
+      case node' of
+        ReturnF r -> ReturnF r
+        EffectF eff -> EffectF (fmap (loop z) eff)
+        ConnectF con ->
+          case fdecompReplaceIdx n (Proxy :: Proxy (Yielding b)) con of
+            Left other -> ConnectF (fmap (loop z) other)
+            Right (a, rest) ->
+              getNode $ do
+                !z' <- lift $ comb z a
+                b <- lift $ fin z'
+                yieldOn n' b
+                loop z' rest
+
+-- $list
+--
+-- __Do not use these in production code!__
+--
+-- These functions do not reflect intended usage of this library, and
+-- are provided only to simplify testing.
+
+-- | Gather yielded values into a list.
+toListN :: Monad m => Producer a m r -> m ([a], r)
+toListN = foldNode (\diff a -> diff . (a :)) id ($ [])
+
+-- | Gather yielded values into a list, and throw away return value.
+toListN_ :: Monad m => Producer a m r -> m [a]
+toListN_ = fmap fst . toListN
