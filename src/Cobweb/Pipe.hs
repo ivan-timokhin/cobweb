c818ccceded26053423143639d49ed92de2b0e77
Ivan Timokhin <timokhin.iv@gmail.com>
2017-09-11 22:50:23 +0300

    Flesh out Pipe
    I may have forgotten what I wanted to put here, so let's just have the
    basics.

--- src/Cobweb/Pipe.hs
+++ src/Cobweb/Pipe.hs
@@ -1,18 +1,66 @@
+{-|
+Module: Cobweb.Pipes
+Description: Nodes that yield and await values.
+Copyright: 2017 © Ivan Timokhin
+License: BSD3
+Maintainer: timokhin.iv@gmail.com
+Stability: experimental
+
+Nodes yield and await values.
+
+-}
+{-# OPTIONS_HADDOCK show-extensions #-}
 {-# LANGUAGE DataKinds #-}
 
-module Cobweb.Pipe where
+module Cobweb.Pipe
+  ( Pipe
+  , await
+  , yield
+    -- * Special pipes
+  , cat
+  , mapping
+  , mappingM
+  , taking
+  , dropping
+  ) where
 
 import Control.Monad (forever, replicateM_)
+import Control.Monad.Trans (lift)
 
-import Cobweb.Core (Awaiting, Yielding, awaitOn, yieldOn)
-import Cobweb.Internal (Node)
-import Cobweb.Type.Combinators (i0, i1)
+import Cobweb.Consumer (await)
+import Cobweb.Core (Pipe)
+import Cobweb.Producer (yield)
 
-takeW :: Functor m => Int -> Node '[ Awaiting a, Yielding a] m ()
-takeW n = replicateM_ n $ awaitOn i0 >>= yieldOn i1
+-- | Identity pipe, simply forwards the values received to the output
+-- channel.
+cat :: Functor m => Pipe a a m r
+cat =
+  forever $ do
+    x <- await
+    yield x
 
-mapping :: Functor m => (a -> b) -> Node '[ Awaiting a, Yielding b] m r
+-- | Apply a function to all values passing through.
+mapping :: Functor m => (a -> b) -> Pipe a b m r
 mapping f =
   forever $ do
-    a <- awaitOn i0
-    yieldOn i1 (f a)
+    a <- await
+    yield (f a)
+
+-- | Apply a function with possible side effects to all values passing
+-- through.
+mappingM :: Monad m => (a -> m b) -> Pipe a b m r
+mappingM f =
+  forever $ do
+    a <- await
+    b <- lift (f a)
+    yield b
+
+-- | Let through a specified number of values, then terminate.
+taking :: Functor m => Int -> Pipe a a m ()
+taking n = replicateM_ n $ await >>= yield
+
+-- | Skip a specified number of values, forward the rest.
+dropping :: Functor m => Int -> Pipe a a m ()
+dropping n = do
+  replicateM_ n await
+  cat
