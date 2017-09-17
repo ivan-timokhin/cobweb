5f6d64134b9ad603a2b971ea5c1a64b5c2d3e992
Ivan Timokhin <timokhin.iv@gmail.com>
2017-09-17 23:52:59 +0300

    Create a Duplex module
    It is essentially a rip-off of the Pipes.Core.
    
    The bad part is that I still have no good ideas for concise examples
    of usage.  For that matter, I'm not entirely sure what is the use-case
    for duplex communication in general.

---
+++ src/Cobweb/Duplex.hs
@@ -0,0 +1,274 @@
+{-|
+Module: Cobweb.Duplex
+Description: Duplex communications, coroutine style.
+Copyright: 2017 © Ivan Timokhin
+License: BSD3
+Maintainer: timokhin.iv@gmail.com
+Stability: experimental
+
+Duplex communications, as provided in this module and supporting
+section in "Cobweb.Link", work on a request-response model.
+
+That is, at each moment, one of the two communicating nodes is
+blocked, waiting for data from the other.  The other node, which is
+currently running, provides that data by making a request to the first
+node, and blocks, waiting for a response.  Now the first node, which
+has received the data it was waiting for, unblocks.  It then runs
+until it makes a request to the second node, providing it the data
+that it was waiting for, and blocking.  And so on.
+
+Now, while this description is entirely correct for the case of
+'Yielding'/'Awaiting' interface, provided by this module as 'Request',
+"Cobweb.Link" in fact allows a more generic duplex interface, with
+arbitrary functors for requests and responses.  Schematically, the
+resulting control flow looks like this:
+
+@
+   .   .   .                        .    .    .
+|  connectsOn  |                     do stuff
+|              |                 ________________
+|_____________ lreq ---><--- rresp              |
+                                 |              |
+    do stuff                     |  connectsOn  |
+________________                 |              |
+|              lresp ---><--- rreq______________|
+|              |
+|  connectsOn  |                     do stuff
+   .   .   .                       .    .    .
+@
+
+Note that now the channel consists of /two/ connection functors—for
+request and response—which are combined via 'Compose'.
+-}
+{-# OPTIONS_HADDOCK show-extensions #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TypeOperators #-}
+
+module Cobweb.Duplex where
+
+import Control.Monad ((>=>))
+import Control.Monad.Trans (lift)
+import Data.Functor.Compose (Compose(Compose))
+import Data.Type.Length (Length)
+import Type.Class.Known (Known)
+import Type.Family.List (type (++))
+
+import Cobweb.Core
+       (Awaiting, Leaf, Node, Tube, Yielding, connectsOn, forsOn, i0, i1)
+import Cobweb.Type.Combinators (All, IIndex, IWithout)
+
+-- | A type of duplex channel, producing values of type @o@ and
+-- awaiting values of type @i@ in response.
+--
+-- Another way to say this, whenever a 'Node' initiates connection on
+-- such channel, it makes a request of the type @o@ and awaits
+-- response of type @i@.
+type Request o i = Yielding o `Compose` Awaiting i
+
+-- | A 'Node' with a single duplex channel, that makes requests of
+-- type @a@, awaiting @b@ in response.
+type Client a b = Leaf (Request a b)
+
+-- | A middleman in a duplex pipeline, exchanges @a'@ for @a@ on an
+-- upstream interface, and @b@ for @b'@ on downstream.
+--
+-- This type is isomorphic to @Proxy@ from @pipes@.
+type Proxy a' a b' b = Tube (Request a' a) (Request b b')
+
+-- | Send a request on the channel with a specified index.
+requestOn :: IIndex n cs (Request a b) -> a -> Node cs m b
+requestOn n x = connectsOn n (Compose (x, id))
+
+-- | Use the provided function to serve incoming requests.
+--
+-- The type is set up so that this function with "Cobweb.Link" duplex
+-- linking functions; e.g. as a first argument to
+-- @('Cobweb.Link.+>>')@ or second to @('Cobweb.Link.>>~')@.
+serve :: Monad m => (a -> m b) -> a -> Client b a m r
+serve f = loop
+  where
+    loop a = do
+      b <- lift (f a)
+      a' <- requestOn i0 b
+      loop a'
+
+-- | Identity for the @('Cobweb.Link.>~>')@ composition operator; that
+-- is,
+--
+-- @
+-- node 'Cobweb.Link.>~>' 'push' = 'push' 'Cobweb.Link.>~>' node = node
+-- @
+--
+-- The following diagram shows the flow of data (it loops
+-- indefinitely).
+--
+-- @
+--               a
+--               |
+--      +--------+--------+
+--      |        |        |
+--      |        \\==========> a
+--      |                 |
+--      |   'requestOn' 'i1'  |
+--      |                 |
+--      |        /==========< a'
+--      |        |        |
+--      +--------+--------+
+--               |
+--      +--------+--------+
+--      |        |        |
+-- a' <==========/        |
+--      |                 |
+--      |   'requestOn' 'i0'  |
+--      |                 |
+--  a >==========\\        |
+--      |        |        |
+--      +--------+--------+
+--               |
+--              ...
+-- @
+push :: Monad m => a -> Proxy a' a a' a m r
+push = loop
+  where
+    loop = requestOn i1 >=> requestOn i0 >=> loop
+
+-- | Identity for the @('Cobweb.Link.>+>')@ composition operator; that
+-- is,
+--
+-- @
+-- node 'Cobweb.Link.>+>' 'pull' = 'pull' 'Cobweb.Link.>+>' node = node
+-- @
+--
+-- The following diagram shows the flow of data (it loops
+-- indefinitely).
+--
+-- @
+--               a'
+--               |
+--      +--------+--------+
+--      |        |        |
+-- a' <==========/        |
+--      |                 |
+--      |   'requestOn' 'i0'  |
+--      |                 |
+--  a >==========\\        |
+--      |        |        |
+--      +--------+--------+
+--               |
+--      +--------+--------+
+--      |        |        |
+--      |        \\==========> a
+--      |                 |
+--      |   'requestOn' 'i1'  |
+--      |                 |
+--      |        /==========< a'
+--      |        |        |
+--      +--------+--------+
+--               |
+--              ...
+-- @
+pull :: Monad m => a' -> Proxy a' a a' a m r
+pull = loop
+  where
+    loop = requestOn i0 >=> requestOn i1 >=> loop
+
+-- | A generalisation of 'push'; instead of simply passing values back
+-- and forth, apply monadic transformation in transit.
+--
+-- The data flow for @'pushProxy' fwd bwd@ looks like this:
+--
+-- @
+--               a
+--               |
+--              fwd
+--               |
+--      +--------+--------+
+--      |        |        |
+--      |        \\==========> b
+--      |                 |
+--      |   'requestOn' 'i1'  |
+--      |                 |
+--      |        /==========< b'
+--      |        |        |
+--      +--------+--------+
+--               |
+--              bwd
+--               |
+--      +--------+--------+
+--      |        |        |
+-- a' <==========/        |
+--      |                 |
+--      |   'requestOn' 'i0'  |
+--      |                 |
+--  a >==========\\        |
+--      |        |        |
+--      +--------+--------+
+--               |
+--              fwd
+--               |
+--              ...
+-- @
+pushProxy :: Monad m => (a -> m b) -> (b' -> m a') -> a -> Proxy a' a b' b m r
+pushProxy fwd bwd = loop
+  where
+    loop =
+      (lift . fwd) >=> requestOn i1 >=> (lift . bwd) >=> requestOn i0 >=> loop
+
+-- | A generalisation of 'pull'; instead of simply passing values back
+-- and forth, apply monadic transformation in transit.
+--
+-- The data flow for @'pullProxy' fwd bwd@ looks like this:
+--
+-- @
+--               b'
+--               |
+--              bwd
+--               |
+--      +--------+--------+
+--      |        |        |
+-- a' <==========/        |
+--      |                 |
+--      |   'requestOn' 'i0'  |
+--      |                 |
+--  a >==========\\        |
+--      |        |        |
+--      +--------+--------+
+--               |
+--              fwd
+--               |
+--      +--------+--------+
+--      |        |        |
+--      |        \\==========> b
+--      |                 |
+--      |   'requestOn' 'i1'  |
+--      |                 |
+--      |        /==========< b'
+--      |        |        |
+--      +--------+--------+
+--               |
+--              bwd
+--               |
+--              ...
+-- @
+pullProxy :: Monad m => (a -> m b) -> (b' -> m a') -> b' -> Proxy a' a b' b m r
+pullProxy fwd bwd = loop
+  where
+    loop =
+      (lift . bwd) >=> requestOn i0 >=> (lift . fwd) >=> requestOn i1 >=> loop
+
+-- | Iterate over specified duplex channel, replacing calls to
+-- 'requestOn' with the loop body.
+forOnDuplex ::
+     ( Monad m
+     , All Functor cs
+     , IWithout n cs cs''
+     , All Functor cs'
+     , Known Length cs''
+     )
+  => IIndex n cs (Request a b) -- ^ Index of the channel.
+  -> Node cs m r
+  -> (a -> Node cs' m b) -- ^ Replacement for 'requestOn'.
+  -> Node (cs'' ++ cs') m r
+forOnDuplex n node f = forsOn n node body
+  where
+    body (Compose (a, fb)) = fmap fb (f a)
